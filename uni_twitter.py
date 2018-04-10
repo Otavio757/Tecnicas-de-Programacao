ac5221731c270a7cbf14f5bc8e8cc6b828d5283c
trlthiago <lolthiago@gmail.com>
2018-04-09 21:03:35 -0300

    Fixing twitter call

--- uni_twitter.py
+++ uni_twitter.py
@@ -1,13 +1,17 @@
+from twitter import *
+
 def load_twitter_tokens():
-    f = open('twitter_settings.txt', 'r')
-    settings = f.split(",")
-    return settings
+    with open('twitter_settings.txt', 'r') as content_file:
+        content = content_file.read()
+        settings = content.split(",")
+        return settings
 
 def start_twitter_monitoring():
     pass
 
 def single_query_twitter(hashtag):
     settings = load_twitter_tokens()
+    print(settings)
     t = Twitter(auth=OAuth(settings[0], settings[1], settings[2], settings[3]))
 
     # Get your "home" timeline
