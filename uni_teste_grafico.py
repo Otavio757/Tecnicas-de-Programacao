7a39ecf8e2e84e06e2432709baf9c23ce527598b
Luis Padilha <lufesp777@gmail.com>
2018-04-17 16:40:07 -0300

    arquivo modificado

--- uni_teste_grafico.py
+++ uni_teste_grafico.py
@@ -1,1 +1,31 @@
 #teste com MATPLOTLIB
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3]
+y = [10,20,30]
+y2 = [15,10,40]
+y3=[20,10,35]
+yBar=[3,10,7,5,3,4.5,6,8.1]
+
+z=[i*1.5 for i in yBar]
+xBar = range(len(yBar))
+
+azul="blue"
+verde="green"
+preto="black"
+
+
+def plota_linha_1():
+    plt.plot(x,y)
+
+
+def plota_linha_2():
+	plt.xlabel('X')
+	plt.ylabel('Y')
+	plt.title('Título do gráfico')
+	plt.plot(x,y)
+	plt.plot(x,y)
+
+
+
