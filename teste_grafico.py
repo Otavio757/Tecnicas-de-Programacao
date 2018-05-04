bd8db826f856c602688afc42edb7ca507dd5e0c6
luis_padilha0856 <LFPProgrammer@users.noreply.github.com>
2018-05-04 01:36:43 -0300

    Ajuste Script para gerar gráfico barra
    Ajuste Script para gerar gráfico barra.
    Ajustes iniciais.

--- teste_grafico.py
+++ teste_grafico.py
@@ -5,34 +5,23 @@
 ## pandas
 ## numpy
 ## pyplot importado do matplotlib
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Abaixo, alguma variáveis a serem usadas
-## os vetores x e y
-
-# pylab.arange(inicio, fim, passo) - define um 'arranjo' com os valores de entrada.
-x = [0, 20, 1]
-y = [10,20,30]
-y2 = [15,10,40]
-y3 = [20,10,35]
-### ACIMA
-
-## arranjo de resultados
-yBar = [3,10,7,6,58,56,12,3]
-z = [i * 1.5 for i in yBar]
-xBar = range(len(yBar))
+'''
+yBar e xBar ainda em testes
+'''
+yBar = [23,23,23] ### EIXO Y COM VALORES FIXOS
+xBar = ['um', 'dois', 'tres']
+### 'quatro', 'cinco', 'seis', 'sete', 'oito'
+## FIXO X COM VALORES NOMES FIXOS
 
 azul = "blue"
 verde = "green"
 preto = "black"
 ## ACIMA
-# cores do grafico
+# cores para serem usadas no grafico
 
-plt.bar(xBar, yBar, color=azul)
+plt.bar(xBar, yBar, color=verde) ## plota as barras e define as cores
 
-plt.show()
-# mostra o gráfico na tela
+plt.show() # mostra o gráfico na tela
