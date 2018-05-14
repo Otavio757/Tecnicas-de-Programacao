edec7c055414a8b56f39c113a3028889f65a64fe
Thiago Roberto <lolthiago@gmail.com>
2018-05-14 20:52:06 -0300

    Updating requirements.txt

--- concepa/Chart.py
+++ concepa/Chart.py
@@ -11,19 +11,44 @@ class obj:
         self.total = total
         self.dayOfWeek = dayOfWeek
 
-results = []
-with open("datasets\\bydate.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE) # change contents to floats
-    for row in reader: # each row is a list
-        results.append(obj(row[0], row[1], row[2], row[3], row[4]))
+def PlotByDayOfWeek():
+    # day of week Vs day hours - each day of week have 24hour
+    pass
+
+def PlotByDayHourTotal():
+    # Soma o total de cada dia da semana por hora
+    pass
+def PlotByDayHourAverage():
+    # Faz a media de fluxo de cada dia da semana por hora
+    pass
+
+def PlotHoursOfOneDay():
+    x = []
+    y = []
+    results = LoadCsv()
+    for i in range(0, 23):
+        x.append(i)
+        y.append(results[i].flow)
+
+    trace1 = go.Scatter(x=x, y=y)
+    trace2 = go.Bar(x=x, y=y)
+    trace3 = go.Scatter(x=x, y=y, mode='markers', marker=dict(size=y))
+
+    data = [trace1, trace2, trace3]
+
+    plotly.offline.plot(data, filename='bar-line')
+
+def LoadCsv():
+    results = []
+    with open("datasets\\bydate.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE) # change contents to floats
+        for row in reader: # each row is a list
+            results.append(obj(row[0], row[1], row[2], row[3], row[4]))
+    return results
 
 
 #x = [t.age for t in mylist if t.person_id == 10]
-x = []
-y = []
-for i in range(0, 23):
-    x.append(i)
-    y.append(results[i].flow)
+
 
 # plotly.offline.plot({
 #     "data": [Scatter(x=x, y=y)],
@@ -43,16 +68,3 @@ for i in range(0, 23):
 
 ###BUBBLE
 #plotly.offline.plot([go.Scatter(x=x, y=y, mode='markers', marker=dict(size=y))], filename='bubblechart-size')
-
-trace1 = go.Scatter(
-    x=x,
-    y=y
-)
-trace2 = go.Bar(
-    x=x,
-    y=y
-)
-
-trace3 = go.Scatter(x=x, y=y, mode='markers', marker=dict(size=y))
-data = [trace1, trace2, trace3]
-plotly.offline.plot(data, filename='bar-line')
