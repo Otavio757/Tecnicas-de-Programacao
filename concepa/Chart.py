e8a90106e85513093d14c193b63a2a6b7932fd92
Thiago Roberto <lolthiago@gmail.com>
2018-05-14 21:36:44 -0300

    trying group data by day hour

--- concepa/Chart.py
+++ concepa/Chart.py
@@ -1,6 +1,7 @@
 import plotly
 import plotly.graph_objs as go
 from plotly.graph_objs import Scatter, Layout
+import numpy
 
 import csv
 
@@ -13,7 +14,31 @@ class obj:
 
 def PlotByDayOfWeek():
     # day of week Vs day hours - each day of week have 24hour
-    pass
+
+    import numpy
+    matrix = numpy.zeros((24, 7))
+
+    # results = []
+    with open("datasets\\bydate.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE) # change contents to floats
+        for row in reader: # each row is a list
+            #results.insert(row[1], obj(row[0], row[1], row[2], row[3], row[4]))
+            # print(str(row[1]+1)
+            # print(str(row[4]+1))
+            i = int(row[1])+1
+            j = int(row[4])
+            #print(matrix[i][j])
+            #matrix[i][j] += row[3]
+            type(matrix[i][j])
+    
+    # x = []
+    # y = []
+    # results = LoadCsv()
+    # for i in range(0, 6):
+    #     x.append(i)
+    #     y.append(results[i].flow)
+
+    plotly.offline.plot({"data":  [go.Bar(matrix)], "layout": Layout(title="Freeway Load") })
 
 def PlotByDayHourTotal():
     # Soma o total de cada dia da semana por hora
@@ -68,3 +93,5 @@ def LoadCsv():
 
 ###BUBBLE
 #plotly.offline.plot([go.Scatter(x=x, y=y, mode='markers', marker=dict(size=y))], filename='bubblechart-size')
+
+PlotByDayOfWeek()
